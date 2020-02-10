4da073e9da0d6a5dce9a8ed9417de6b657481db3
CY <w.connor.yates@gmail.com>
2020-02-10 16:01:15 -0500

    Git test and pushing changes.

--- robot/vision.py
+++ robot/vision.py
@@ -4,15 +4,17 @@ import wpilib
 from networktables import NetworkTables
 import math
 
+
 class Vision:
     def __init__(self):
         self.heightCamera = 0 # change later
         self.heightTarget = 0 # change later
         self.angleMount = 0 # change later
-
-        self.verticalAngleToTarget = NetworkTables.getTable("limelight").getNumber('ty') # finds vertical angle to target
-        self.horizontalAngleToTarget = NetworkTables.getTable("limelight").getNumber('tx') # finds horizontal angle to target
+        self.limelight = NetworkTables.getTable("limelight")
 
     def getDistance(self):
+        self.verticalAngleToTarget = self.limelight.getNumber('ty')  # finds vertical angle to target
+        self.horizontalAngleToTarget = self.limelight.getNumber('tx')  # finds horizontal angle to target
+
         # finds distance to target using limelight
         self.distanceToTarget = (self.heightTarget - self.heightCamera) / math.tan(self.angleMount + self.angleToTarget)
